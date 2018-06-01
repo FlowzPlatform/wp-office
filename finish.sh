97123904daa81feb76ceee47875bbbb94d9f14af
software <kavib@officebrain.com>
2018-06-01 16:35:46 +0530

    Travis-CI Files

--- finish.sh
+++ finish.sh
@@ -1,10 +1,34 @@
 
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
+    {
+    echo "call $TRAVIS_BRANCH branch"
+    ENV_ID=`curl -u ""$RANCHER_ACCESSKEY_MASTER":"$RANCHER_SECRETKEY_MASTER"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_MASTER/v2-beta/projects?name=uat" | jq '.data[].id' | tr -d '"'`
+    echo $ENV_ID
 
-ENV_ID=`curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://209.50.52.18:8080/v2-beta/projects?name=uat" | jq '.data[].id' | tr -d '"'`
-echo $ENV_ID
+    SERVICE_ID=`curl -u ""$RANCHER_ACCESSKEY_MASTER":"$RANCHER_SECRETKEY_MASTER"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_MASTER/v2-beta/projects/$ENV_ID/services?name=officebeacon-uat-demo" | jq '.data[].id' | tr -d '"'`
+    echo $SERVICE_ID
 
-SERVICE_ID=`curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://209.50.52.18:8080/v2-beta/projects/$ENV_ID/services?name=officebeacon-uat" | jq '.data[].id' | tr -d '"'`
-echo $SERVICE_ID
+    RANCHER_ACCESSKEY="$RANCHER_ACCESSKEY_MASTER";
+    RANCHER_SECRETKEY="$RANCHER_SECRETKEY_MASTER";
+    RANCHER_URL="$RANCHER_URL_MASTER";
+  }
+elif [ "$TRAVIS_BRANCH" = "develop" ]
+then
+    {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_ACCESSKEY_DEVELOP":"$RANCHER_SECRETKEY_DEVELOP"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_DEVELOP/v2-beta/projects?name=uat" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+
+    SERVICE_ID=`curl -u ""$RANCHER_ACCESSKEY_DEVELOP":"$RANCHER_SECRETKEY_DEVELOP"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_DEVELOP/v2-beta/projects/$ENV_ID/services?name=officebeacon-uat" | jq '.data[].id' | tr -d '"'`
+    echo $SERVICE_ID
+
+      RANCHER_ACCESSKEY="$RANCHER_ACCESSKEY_DEVELOP";
+      RANCHER_SECRETKEY="$RANCHER_SECRETKEY_DEVELOP";
+      RANCHER_URL="$RANCHER_URL_DEVELOP";
+  }
+fi
+
 
 echo "waiting for service to upgrade "
     while true; do
@@ -13,14 +37,14 @@ echo "waiting for service to upgrade "
           -X GET \
           -H 'Accept: application/json' \
           -H 'Content-Type: application/json' \
-          "http://209.50.52.18:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID/" | jq '.state'` in
+          "$RANCHER_URL/v2-beta/projects/$ENV_ID/services/$SERVICE_ID/" | jq '.state'` in
           "\"upgraded\"" )
               echo "completing service upgrade"
               curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
                 -X POST \
                 -H 'Accept: application/json' \
                 -H 'Content-Type: application/json' \
-                "http://209.50.52.18:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID?action=finishupgrade"
+                "$RANCHER_URL/v2-beta/projects/$ENV_ID/services/$SERVICE_ID?action=finishupgrade"
               break ;;
           "\"upgrading\"" )
               echo "still upgrading"
@@ -31,7 +55,5 @@ echo "waiting for service to upgrade "
               die "unexpected upgrade state" ;;
       esac
     done
-
-
 
 
--- upgrade.sh
+++ upgrade.sh
@@ -1,10 +1,36 @@
+if [ "$TRAVIS_BRANCH" = "master" ]
+then
+    {
+    echo "call $TRAVIS_BRANCH branch"
+    ENV_ID=`curl -u ""$RANCHER_ACCESSKEY_MASTER":"$RANCHER_SECRETKEY_MASTER"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_MASTER/v2-beta/projects?name=uat" | jq '.data[].id' | tr -d '"'`
+    echo $ENV_ID
 
+    SERVICE_ID=`curl -u ""$RANCHER_ACCESSKEY_MASTER":"$RANCHER_SECRETKEY_MASTER"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_MASTER/v2-beta/projects/$ENV_ID/services?name=officebeacon-uat-demo" | jq '.data[].id' | tr -d '"'`
+    echo $SERVICE_ID
 
-ENV_ID=`curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://209.50.52.18:8080/v2-beta/projects?name=uat" | jq '.data[].id' | tr -d '"'`
-echo $ENV_ID
+    TAG="master";
+    HOST="$HOST_FOR_MASTER";
+    RANCHER_ACCESSKEY="$RANCHER_ACCESSKEY_MASTER";
+    RANCHER_SECRETKEY="$RANCHER_SECRETKEY_MASTER";
+    RANCHER_URL="$RANCHER_URL_MASTER";
+  }
+elif [ "$TRAVIS_BRANCH" = "develop" ]
+then
+    {
+      echo "call $TRAVIS_BRANCH branch"
+      ENV_ID=`curl -u ""$RANCHER_ACCESSKEY_DEVELOP":"$RANCHER_SECRETKEY_DEVELOP"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_DEVELOP/v2-beta/projects?name=uat" | jq '.data[].id' | tr -d '"'`
+      echo $ENV_ID
+      
+      SERVICE_ID=`curl -u ""$RANCHER_ACCESSKEY_DEVELOP":"$RANCHER_SECRETKEY_DEVELOP"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_DEVELOP/v2-beta/projects/$ENV_ID/services?name=officebeacon-uat" | jq '.data[].id' | tr -d '"'`
+      echo $SERVICE_ID
 
-SERVICE_ID=`curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "http://209.50.52.18:8080/v2-beta/projects/$ENV_ID/services?name=officebeacon-uat" | jq '.data[].id' | tr -d '"'`
-echo $SERVICE_ID
+      TAG="dev";
+      HOST="$HOST_FOR_DEVELOP";
+      RANCHER_ACCESSKEY="$RANCHER_ACCESSKEY_DEVELOP";
+      RANCHER_SECRETKEY="$RANCHER_SECRETKEY_DEVELOP";
+      RANCHER_URL="$RANCHER_URL_DEVELOP";
+  }
+fi
 
 
 curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
@@ -14,15 +40,15 @@ curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
 -d '{
      "inServiceStrategy":{
            "launchConfig": {
-                       "imageUuid":"docker:obdev/flz-wdp-v6-demo:new",
+                       "imageUuid":"docker:'$DOCKER_USERNAME_OBDEV'/flz-wdp-v6-demo:'$TAG'",
                        "kind": "container",
                        "labels":{
                                "io.rancher.container.pull_image": "always",
-                               "io.rancher.scheduler.affinity:host_label": "machine=wordpress"
+                               "io.rancher.scheduler.affinity:host_label": "'"$HOST"'"
                                 },
                         "networkMode": "managed"
                         }
         },
      "toServiceStrategy":null
     }' \
-http://209.50.52.18:8080/v2-beta/projects/$ENV_ID/services/$SERVICE_ID?action=upgrade
+$RANCHER_URL/v2-beta/projects/$ENV_ID/services/$SERVICE_ID?action=upgrade
