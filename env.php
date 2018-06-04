5a3fb86dac298f725a5786c88acef3d8ff6d4181
software <kavib@officebrain.com>
2018-06-04 13:57:08 +0530

    1

--- env.php
+++ env.php
@@ -7,7 +7,7 @@ $ip = $_SERVER['REMOTE_ADDR'];
 
 // Safely get the value of an environment variable, ignoring whether 
 // or not it was set by a SAPI or has been changed with putenv
-$ip = getenv('REMOTE_ADDR', true) ?: getenv('REMOTE_ADDR')
+$ip = getenv('REMOTE_ADDR', true) ?: getenv('REMOTE_ADDR');
 
 
 echo $ip;
